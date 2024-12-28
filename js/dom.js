7e669bdeebc226c525bbf6f768c344e2bae15ea8
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2024-12-28 23:07:27 +0700

    Create dom selector

---
+++ js/dom.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.btn');
+const display = document.querySelector('.display');
+const number = document.querySelector('#number');
+const form = document.querySelector('form');
+
+// get the user input
+const numVal = Number(number.value);
+
+export { numVal, btn, display, form };
