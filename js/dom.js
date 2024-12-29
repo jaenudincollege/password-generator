df77c6524142d58bf7c38ba175e7458eb31625d3
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2024-12-29 09:49:39 +0700

    Add new selector for qrcode

--- js/dom.js
+++ js/dom.js
@@ -2,8 +2,10 @@ const display = document.querySelector('.display');
 const number = document.querySelector('#number');
 const form = document.querySelector('form');
 const year = document.querySelector('.year');
+const qrBox = document.querySelector('.qr-box');
+const passVal = document.querySelector('.display');
 
 const date = new Date();
 year.textContent = date.getFullYear();
 
-export { number, display, form };
+export { number, display, form, qrBox, passVal };
