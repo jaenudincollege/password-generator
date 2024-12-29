879c331743505ed2ab559e51e5928363caaa371f
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2024-12-29 09:20:09 +0700

    Create dynamic year

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,9 @@
 const display = document.querySelector('.display');
 const number = document.querySelector('#number');
 const form = document.querySelector('form');
+const year = document.querySelector('.year');
+
+const date = new Date();
+year.textContent = date.getFullYear();
 
 export { number, display, form };
