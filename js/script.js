9b2dd58e014093442e1cdbbbfdafe6c89dc78ff0
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2024-12-29 09:51:48 +0700

    Implement qrcode generator

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 import password from './password-array.js';
-import { number, form, display } from './dom.js';
+import { number, display, form, qrBox, passVal } from './dom.js';
 import getRandomItems from './logic.js';
+import generateQRCode from './qrcode.js';
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const randomPassword = getRandomItems(password, number.value);
   display.textContent = randomPassword;
+  generateQRCode(qrBox, randomPassword);
 });
-
-// TODO: add qr code from generate random password
